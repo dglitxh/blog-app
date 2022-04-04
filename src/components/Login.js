9ff09daa2d64b87f95e00144f577c2d5c9c17677
YD <ydglitch@gmail.com>
2022-04-04 21:43:42 +0000

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import { Button, TextInput } from 'react-native-paper';
 import { ScrollView, View, Image, Text } from 'react-native';
 import img from '../assets/svgs/login.svg';
 
-const svg = require("../assets/svgs/login.svg")
+
 
 const Login = () => {
   const [mail, setMail] = useState('')
@@ -12,10 +12,10 @@ const Login = () => {
   
 
   return (
-    
+    <ScrollView>
     <View style={{flex: 3, flexDirection: 'column', justifyContent: 'center', padding:15}}>
 
-  <Image source={svg} style={{width: 10}} />
+  <Image source={require("../assets/imgs/forgot.png")} style={{width: 10}} />
   <Text style={{fontSize: 49, fontWeight: 'bold', color: 'green'}}> Login </Text>
     <TextInput
       label="Email"
@@ -34,7 +34,7 @@ const Login = () => {
     <Text style={{paddingTop: 6, color: 'green', display: 'flex'}}>forgot password?</Text>
     <Button color='green'> login with google</Button>
     </View>
-    
+    </ScrollView>
      
   );
 }
