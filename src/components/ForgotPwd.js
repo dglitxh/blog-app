9d6a50b3f7418dd16f5f9432ee91fd1d8b0e7d9b
YD <ydglitch@gmail.com>
2022-04-07 13:14:09 +0000

    forgotPwd: ui

--- src/components/ForgotPwd.js
+++ src/components/ForgotPwd.js
@@ -1,21 +1,27 @@
 
 import React, { useState } from 'react';
 import { Button, TextInput } from 'react-native-paper';
-import { ScrollView, View, Image, Text } from 'react-native';
+import {  View,  Text } from 'react-native';
+import Img from '../assets/svgs/login.svg';
+
 
 
 const Forgot = () => {
 
     return(
         <View style={{flex: 3, flexDirection: 'column', justifyContent: 'center', padding:15}}>
+            <Img width={300} height={200} />
             <Text style={{fontSize: 49, fontWeight: 'bold', color: 'green'}}> Forgot </Text>
             <Text style={{fontSize: 49, fontWeight: 'bold', color: 'green'}}> Password? </Text>
                 <TextInput
                 label="Email"
                 value={mail}
-                mode= {'outlined'}
+                mode= {'flat'}
+                activeUnderlineColor="green"
+                backgroundColor="white"
                 onChangeText={mail => setMail(mail)}
                 />
+                <Button mode='contained'> Submit </Button>
         </View>
     )
 }
