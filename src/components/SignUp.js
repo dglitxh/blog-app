5cbcdb3b48fac0fb3a915e8b32d3272d9e8ac34c
YD <ydglitch@gmail.com>
2022-04-07 13:07:39 +0000

    signup: ui

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Button, TextInput } from 'react-native-paper';
 import { ScrollView, View, Image, Text } from 'react-native';
-
+import Img from '../assets/svgs/auth.svg';
 
 
 
@@ -11,23 +11,26 @@ const SignUp = () => {
   const [password1, setPassword1] = useState('')
   const [password2, setPassword2] = useState('')
  
-  
-
   return (
     <ScrollView>
     <View style={{flex: 3, flexDirection: 'column', justifyContent: 'center', padding:15}}>
+    <Img width={300} height={200} />
   <Text style={{fontSize: 49, fontWeight: 'bold', color: 'green'}}> SignUp </Text>
     <TextInput
       label="Name"
       value={username}
-      mode= {'outlined'}
+      mode= {'flat'}
+      activeUnderlineColor="green"
+      backgroundColor="white"
       onChangeText={setUsername => setUsername(username)}
     />
   <Text style={{fontSize: 49, fontWeight: 'bold', color: 'green'}}> SignUp </Text>
     <TextInput
       label="Email"
       value={mail}
-      mode= {'outlined'}
+      mode= {'flat'}
+      activeUnderlineColor="green"
+      backgroundColor="white"
       onChangeText={mail => setMail(mail)}
     />
 
@@ -35,7 +38,9 @@ const SignUp = () => {
       label="password"
       value={password1}
       secureTextEntry
-      mode= {'outlined'}
+      mode= {'flat'}
+      activeUnderlineColor="green"
+      backgroundColor="white"
       onChangeText={password1 => setPassword1(password1)}
     />
 
@@ -43,12 +48,13 @@ const SignUp = () => {
       label="repeat password"
       value={password2}
       secureTextEntry
-      selectionColor=''
-      mode= {'outlined'}
+      mode= {'flat'}
+      activeUnderlineColor="green"
+      backgroundColor="white"
       onChangeText={password2 => setPassword2(password2)}
     />
-    <Text style={{paddingTop: 6, color: 'green', display: 'flex'}}>forgot password?</Text>
     <Button color='green'> login with google</Button>
+    <Text style={{textAlign: "center", paddingTop: 24}}>Have an account? <Text style={{color: "green"}}>Login</Text> </Text>
     </View>
     </ScrollView>
      
