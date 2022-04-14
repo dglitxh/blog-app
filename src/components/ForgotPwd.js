b777319183fcb0cce0030cf7872a45416cf83cf6
YD <ydglitch@gmail.com>
2022-04-14 08:40:51 +0000

    forgotPwd: api

--- src/components/ForgotPwd.js
+++ src/components/ForgotPwd.js
@@ -9,6 +9,25 @@ import Img from '../assets/svgs/forgot.svg';
 const Forgot = () => {
     const [mail, setMail] = useState('')
     
+    const requestPwdChg = async () => {
+        const settings = {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: mail
+            })
+        }
+        try{
+            const response = await fetch("https://iyceblog.herokuapp.com", settings)
+            const data  = await response.json()
+            console.log(data)
+        }catch(e){
+
+        }
+    }
     return(
         <View style={{backgroundColor: "white", flex: 3, flexDirection: 'column', justifyContent: 'center', padding:15}}>
             <Img width={300} height={200} />
@@ -22,7 +41,7 @@ const Forgot = () => {
                 backgroundColor="white"
                 onChangeText={mail => setMail(mail)}
                 />
-                <Button style={{margin: 20, backgroundColor: "green",}} mode='contained'> Submit </Button>
+                <Button onPress={() => requestPwdChg} style={{margin: 20, backgroundColor: "green",}} mode='contained'> Submit </Button>
         </View>
     )
 }
