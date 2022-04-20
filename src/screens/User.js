e26db7977b2afb87136b412e2ded331019b0cc30
YD <ydglitch@gmail.com>
2022-04-20 13:58:31 +0000

    user screen

--- src/screens/User.js
+++ src/screens/User.js
@@ -8,11 +8,22 @@ const Stack = createNativeStackNavigator();
 
 function UserStack() {
   return (
+    <View>
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="SignUp" component={SignUp} />
-      <Stack.Screen name="Forgot" component={Forgot} />
+      {
+      user?
+        <View>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Forgot" component={Forgot} />
+        </View>
+      :
+        <View>
+          
+        </View>
+      }
     </Stack.Navigator>
+    </View>
   );
 }
 
