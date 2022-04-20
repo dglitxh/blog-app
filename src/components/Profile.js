7ad0625c3f9801a0b9c1884cab1060f9f2bd0691
YD <ydglitch@gmail.com>
2022-04-20 14:54:57 +0000

    profile

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import { View } from "react-native"
+
+const Profile = () => {
+
+    return(
+        <View>
+            <Text>Hi I am a user</Text>
+        </View>
+    )
+}
+
+export default Profile
