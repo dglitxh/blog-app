9588ee084f19a9361cade2cc61ed70580091107e
YD <ydglitch@gmail.com>
2022-04-21 16:56:55 +0000

    Home screen

---
+++ src/screens/Home.js
@@ -0,0 +1,63 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, {useState, useEffect} from 'react'
+import { View } from 'react-native'
+import Posts from '../components/Posts';
+import PostView from '../components/PostView';
+
+const Stack = createNativeStackNavigator();
+
+const HomeStack = () => {
+    const [posts, setPosts] = useState([])
+    const [post, setPost] = useState({})
+
+    useEffect(() => {
+        const getPosts = async () => {
+            try{
+                const fetchPosts = await fetch('http://iyceblog.herokuapp.com/api/posts')
+                const data = await fetchPosts.json()
+                console.log(data.results)
+                setPosts(data.results)
+                alert('got data')
+            }catch(e){
+                console.log('Error', e)
+            }   
+        }
+        getPosts()
+    }, [])
+
+    const PostsComp = ({navigation}) => {
+        return(
+            <View>
+                <Posts navigation={navigation} posts={posts} sendPost={sendPost}/>
+            </View>
+        )
+        
+    }
+
+    function PostComp() {
+        return(
+            <View>
+                <PostView post={post}/>
+            </View>
+        )
+        
+    }
+
+    function addPosts(posts) {
+        setPosts(posts)
+    }
+
+    function sendPost(post) {
+        setPost(post)
+    }
+
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name={"Posts"} component={PostsComp} />
+            <Stack.Screen name={"PostView"} component={PostComp} />
+        </Stack.Navigator>
+    )
+}
+
+
+export default HomeStack
