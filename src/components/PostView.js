684ddf76a008b5d8f5f56f0aefc0b65a2379d501
YD <ydglitch@gmail.com>
2022-04-22 14:02:44 +0000

    added post comments

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -2,13 +2,23 @@ import  React from 'react'
 import { View, ScrollView, Text, Image } from 'react-native'
 
 const PostView = (props) => {
-    const { post } = props
+    const { post, comments } = props
 
     return(
-        <ScrollView>
-            <Text>{post.title}</Text>
+        <ScrollView style={{backgroundColor: "white", padding:15}}>
+            <Text style={{fontSize: 49, fontWeight: 'bold', color: 'green'}}>{post.title}</Text>
             <Image source={{uri: post.thumbnail}}/>
-            <Text>{post.body}</Text>
+            <Text style={{fontSize: 20}}>{post.body}</Text>
+            <View style={{paddingRight: 20}}>
+            <Text> Comments</Text>
+            {
+              comments.map((comment) => {
+                return (
+                  <Text>{comment.body}</Text>
+                )
+              })
+            }
+            </View>
         </ScrollView>
     )
 }
