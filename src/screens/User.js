5dbb8f43658a0bdb53ac23112dcc2400bc5c0605
YD <ydglitch@gmail.com>
2022-04-22 06:24:50 +0000

    refactor

--- src/screens/User.js
+++ src/screens/User.js
@@ -1,32 +1,30 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, {useState} from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import Login from '../components/Login';
 import SignUp from '../components/SignUp';
 import Forgot from '../components/ForgotPwd'
 import Profile from '../components/Profile';
+
 const Stack = createNativeStackNavigator();
+
+const NotUser = () => {
+
+  return (
+    <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Forgot" component={Forgot} />
+    </Stack.Navigator>
+  )
+}
 
 
 function UserStack() {
   const [user, setUser] = useState('')
+  const person = user? <Profile/> : <NotUser/>
   return (
-    <View>
-    <Stack.Navigator>
-      {
-      user?
-        <View>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-          <Stack.Screen name="Forgot" component={Forgot} />
-        </View>
-      :
-        <View>
-          <Stack.Screen name="Profile" component={Profile}/>
-        </View>
-      }
-    </Stack.Navigator>
-    </View>
+    person
   );
 }
 
