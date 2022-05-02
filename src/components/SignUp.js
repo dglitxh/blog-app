5b07e97bd8ceab8ced0dfdde3ece08ccea19a487
YD <ydglitch@gmail.com>
2022-05-02 22:29:59 +0000

    signup: storeData

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import { Button, TextInput } from 'react-native-paper';
 import { ScrollView, View, Text } from 'react-native';
 import Img from '../assets/svgs/auth.svg';
-
+import { storeData } from '../helpers/helpers'
 
 
 const SignUp = ({navigation}) => {
-  const [username, setUsername] = useState('')  
+  const [username, setUsername] = useState('')
   const [mail, setMail] = useState('')
   const [password1, setPassword1] = useState('')
   const [password2, setPassword2] = useState('')
- 
+
   const signupPost = async () => {
     const settings = {
       method: "POST",
@@ -29,12 +29,13 @@ const SignUp = ({navigation}) => {
       const response = await fetch("http://iyceblog.herokuapp.com", settings)
       const data = await response.json()
       console.log(data)
+      storeData(data)
     }catch(err){
       console.log(err)
     }
-    
+
   }
-   
+
 
   return (
     <ScrollView>
@@ -84,7 +85,7 @@ const SignUp = ({navigation}) => {
     style={{textAlign: "center",}}>Have an account? <Text style={{color: "green"}}>Login</Text> </Text>
     </View>
     </ScrollView>
-     
+
   );
 }
 
