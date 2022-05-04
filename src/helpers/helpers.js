7b008dca5a12a336455813c8285c8e1c27f1040a
YD <ydglitch@gmail.com>
2022-05-04 04:21:00 +0000

    helpers: storage

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,7 +1,22 @@
-import { MMKV } from 'react-native-mmkv'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const storage = new MMKV({
-  id: `user-${userId}-storage`,
-  path: `${USER_DIRECTORY}/storage`,
-  encryptionKey: 'hunter2'
-})
+export const storeData = async (key, value) => {
+  try{
+    await AsyncStorage.setItem(key, JSON.stringify(value))
+  }catch(e){
+    console.log("From storeData: data storage failed", e)
+  }
+}
+
+export const getData = async (key) => {
+  try {
+    const val = await AsyncStorage.getItem(key)
+    if (val !== null){
+      return JSON.parse(val)
+    }else{
+      console.log("From getData: data not available", e)
+    }
+  }catch(e){
+    console.log(e)
+  }
+}
