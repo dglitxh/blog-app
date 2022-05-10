051db4124a0a85fb6a6a9e17e36a92b3a7a885d2
YD <ydglitch@gmail.com>
2022-05-10 15:25:00 +0000

    User: userContext

--- src/screens/User.js
+++ src/screens/User.js
@@ -4,7 +4,9 @@ import { View, Text } from 'react-native'
 import Login from '../components/Login';
 import SignUp from '../components/SignUp';
 import Forgot from '../components/ForgotPwd'
-import Profile from '../components/Profile';
+import Profile from '../components/Profile'
+import {useContext} from 'react'
+import {userContext} from '.../App'
 
 const Stack = createNativeStackNavigator();
 
@@ -21,7 +23,7 @@ const NotUser = () => {
 
 
 function UserStack() {
-  const [user, setUser] = useState('')
+  const [user, setUser] = useContext(userContext)
   const authState = user? <Profile/> : <NotUser/>
   return (
     authState
